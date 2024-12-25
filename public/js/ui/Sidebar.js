d4bfc056112d69eea3c4c44a304594cbc11c6f3a
EVM27 <145212059+EVM27@users.noreply.github.com>
2024-12-25 20:23:18 +0300

    1

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -18,7 +18,22 @@ class Sidebar {
    * при нажатии на кнопку .sidebar-toggle
    * */
   static initToggleButton() {
+    const toggleButton = document.querySelector(".sidebar-toggle"); 
+    const body = document.body; 
 
+    if (toggleButton) { 
+      toggleButton.addEventListener("click", (event) => {
+        event.preventDefault(); 
+       
+        if (body.classList.contains("sidebar-open")) {
+          body.classList.remove("sidebar-open");
+          body.classList.add("sidebar-collapse");
+        } else {
+          body.classList.remove("sidebar-collapse");
+          body.classList.add("sidebar-open");
+        }
+      });
+    }
   }
 
   /**
@@ -29,6 +44,37 @@ class Sidebar {
    * выходу устанавливает App.setState( 'init' )
    * */
   static initAuthLinks() {
+    const loginButton = document.querySelector(".menu-item_login");
+    const registerButton = document.querySelector(".menu-item_register");
+    const logoutButton = document.querySelector(".menu-item_logout");
 
+    if (loginButton) {
+      loginButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        const loginModal = App.getModal("login");
+        if (loginModal) loginModal.open();
+      });
+    }
+
+    if (registerButton) {
+      registerButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        const registerModal = App.getModal("register");
+        if (registerModal) registerModal.open();
+      });
+    }
+
+    if (logoutButton) {
+      logoutButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        User.logout((err, response) => {
+          if (response && response.success) {
+            App.setState("init");
+          }
+        });
+      });
+    }
   }
-}
+  }
+//так и не газобрался, почему не работает гамбургер 
+
