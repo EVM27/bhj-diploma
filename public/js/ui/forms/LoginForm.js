af6d1a776498c3b975f5a0d5fea2cdee51e6314f
EVM27 <145212059+EVM27@users.noreply.github.com>
2025-01-04 23:13:18 +0300

    рег и авторез сделал

--- public/js/ui/forms/LoginForm.js
+++ public/js/ui/forms/LoginForm.js
@@ -10,6 +10,18 @@ class LoginForm extends AsyncForm {
    * закрывает окно, в котором находится форма
    * */
   onSubmit(data) {
-
+    User.login(data, (err, response) => {
+      if (response && response.success) {
+        this.element.reset(); 
+        App.setState('user-logged');
+        const modal = this.element.closest('.modal'); 
+        if (modal) {
+          Modal.close(modal.dataset.modalId); 
+        }
+      } else {
+        console.error(err || response.error); 
+        alert('Ошибка авторизации: ' + (response.error || 'Попробуйте снова.')); 
+      }
+    });
   }
 }
--- public/js/ui/forms/RegisterForm.js
+++ public/js/ui/forms/RegisterForm.js
@@ -10,6 +10,18 @@ class RegisterForm extends AsyncForm {
    * и закрывает окно, в котором находится форма
    * */
   onSubmit(data) {
-
+    User.register(data, (err, response) => {
+      if (response && response.success) {
+        this.element.reset();
+        App.setState('user-logged');
+        const modal = this.element.closest('.modal');
+        if (modal) {
+          Modal.close(modal.dataset.modalId);
+        }
+      } else {
+        console.error(err || response.error);
+        alert('Ошибка регистрации: ' + (response.error || 'Попробуйте снова.'));
+      }
+    });
   }
 }
